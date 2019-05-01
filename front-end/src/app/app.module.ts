8fa56bc4af15f382209685b8be88e7228fd38c27
Vladimir Mihov <kols@abv.bg>
2019-05-01 22:33:08 +0300

    Updating root component

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImagesComponent } from './images/images.component';
+import { ImageNavigatorComponent } from './image-navigator/image-navigator.component';
+import { FilterIncludesPipe } from './filter-includes.pipe';
+import { ImageAddComponent } from './image-add/image-add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ImagesComponent,
+    ImageNavigatorComponent,
+    FilterIncludesPipe,
+    ImageAddComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   bootstrap: [AppComponent]
 })
