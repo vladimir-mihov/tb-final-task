be4d73c5166df0ccf758641cd4dc78974928882b
Vladimir Mihov <kols@abv.bg>
2019-05-01 22:33:15 +0300

    Adding routing to the app

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ImageNavigatorComponent } from './image-navigator/image-navigator.component';
+import { ImageAddComponent } from './image-add/image-add.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'add', component: ImageAddComponent },
+    { path: '**', component: ImageNavigatorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
