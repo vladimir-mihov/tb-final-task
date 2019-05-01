08047ec73826731b7e2074783d8b39ed27836822
Vladimir Mihov <kols@abv.bg>
2019-05-01 22:31:06 +0300

    Changing images component to only display images
    Images are passed as input from parent component

--- front-end/src/app/images/images.component.ts
+++ front-end/src/app/images/images.component.ts
@@ -1,25 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { InMemoryCRUDService } from '../in-memory-crud.service';
-import { ImageData } from '../image-data';
+import { Component, Input } from '@angular/core';
+import { ImageData } from '../data/image-data';
 
 @Component({
     selector: 'app-images',
     templateUrl: './images.component.html',
     styleUrls: ['./images.component.css']
 })
-export class ImagesComponent implements OnInit {
+export class ImagesComponent {
     
-    images: ImageData[];
-    visibleSlice = [0,7];
-
-    constructor(private imageService: InMemoryCRUDService) {}
-    
-    ngOnInit() {
-        this.getImages();
-    }
-    
-    getImages() {
-        this.imageService.readAll().subscribe( images => this.images = images );
-    }
+    @Input() images: ImageData[];
+    @Input() page: number;
+    @Input() imagesPerPage: number;
+    @Input() filter: string;
     
 }
